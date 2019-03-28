315089c08b946a3b173e07615f2214e3b0d99e44
Hamza Amer <hamer1563@gmail.com>
2019-03-28 15:22:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var app = express();
 
+app.use(express.static(__dirname));
+
+var dbUrl = "mongodb+srv://heyrio:<password>@cluster0-7cpbz.mongodb.net/test?retryWrites=true"
 
 
 
